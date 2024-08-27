deccb25dde1e9c0ed3bdb1a5a5f40439543c51fd
Homth <hakan.colakoglu.ep@gmail.com>
2024-08-27 11:38:32 +0300

    Simple setup with redis and pg

---
+++ postgres-init/init.sql
@@ -0,0 +1,26 @@
+-- init.sql
+
+-- Step 1: Create a new database, you can handle this 
+-- in docker-compose file too by setting POSTGRES_USER
+CREATE DATABASE your_db_name;
+
+-- Step 2: Create a new role (user) with a password, you can handle this 
+-- in docker-compose file too by setting POSTGRES_PASSWORD
+CREATE ROLE your_db_user WITH LOGIN PASSWORD 'your_db_password';
+
+-- Step 3: Grant privileges to the new user on the new database
+GRANT ALL PRIVILEGES ON DATABASE "your_db_name" TO your_db_user;
+
+-- Step 4: Connect to the newly created database
+\c your_db_name;
+
+-- Step 5: Create the users table
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(100) UNIQUE NOT NULL,
+  password VARCHAR(100) NOT NULL
+);
+
+-- Step 6: Grant privileges on the users table to the new user
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE users TO your_db_user;
+GRANT USAGE, SELECT ON SEQUENCE users_id_seq TO your_db_user;
---
+++ src/app.ts
@@ -0,0 +1,53 @@
+import express from 'express';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import { createClient } from 'redis';
+import { Pool } from 'pg';
+
+import indexRoutes from './routes/index';
+// Load environment variables
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Set up logging
+app.use(morgan('dev'));
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Middleware to parse x-www-form-urlencoded requests
+app.use(express.urlencoded({ extended: true }));
+
+// Initialize Redis client
+const redisClient = createClient({
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+  },
+});
+
+// redisClient.connect()
+//   .then(() => console.log('Redis client connected'))
+//   .catch(err => console.error('Redis connection error:', err));
+
+// Initialize PostgreSQL client
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: Number(process.env.DB_PORT),
+});
+// pool.connect()
+//   .then(() => console.log('Connected to PostgreSQL database'))
+//   .catch(err => console.error('PostgreSQL connection error:', err));
+
+// Routes
+app.use('/', indexRoutes);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+export { app, pool, redisClient };
